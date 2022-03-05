05b0a0bfa0fe86798fa8799695ca5d9e9ddfeb1b
Luan <ltran@asymmetrik.com>
2022-03-05 16:37:06 -0500

    chore: add types to vars

--- pdf_to_ppt/convert.py
+++ pdf_to_ppt/convert.py
@@ -1,4 +1,5 @@
 import os, sys, io, time
+import py_compile
 
 from PIL import Image
 from pptx import Presentation
@@ -6,9 +7,9 @@ from pptx.util import Inches
 from pdf2image import convert_from_path
 
 # decorator for timing
-def time_it(func):
+def time_it(func: function):
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time: float = time.time()
         result = func(*args, **kwargs)
         print(f"{func.__name__} complete! {time.time() - start_time} seconds\n")
         return result
@@ -19,15 +20,15 @@ def time_it(func):
 @time_it
 def convert():
     # load file
-    input_file = sys.argv[1]
-    pages = load_pdf(input_file)
+    input_file: str = sys.argv[1]
+    pages: list = load_pdf(input_file)
 
     # create presentation
     ppt = Presentation()
     create_ppt(ppt, pages)
 
     # save presentation
-    output_file = (
+    output_file: str = (
         f"{input_file.split('.', 1)[0]}.pptx"
         if len(sys.argv) < 3
         else f"{sys.argv[2].split('.', 1)[0]}.pptx"
@@ -36,13 +37,13 @@ def convert():
 
     # print file stats
     print(f"{len(pages)} pages converted to {output_file}")
-    size_in_mb = round(os.stat(output_file).st_size / (1024 * 1024), 2)
+    size_in_mb: int = round(os.stat(output_file).st_size / (1024 * 1024), 2)
     print(f"File size: {size_in_mb} MB\n")
 
 
 # Load PDF
 @time_it
-def load_pdf(file):
+def load_pdf(file: str):
     print(f"Loading {file}...")
 
     # this seems to be the optimal setting without losing too much quality
@@ -52,7 +53,7 @@ def load_pdf(file):
 
 # Crnate slides
 @time_it
-def create_ppt(ppt, pages):
+def create_ppt(ppt: Presentation, pages: list):
     print("Creating Powerpoint...")
 
     # slide config
